f4ac6ca690dfcefda8d327fb8c2f571e701c68a2
Agustin Gibertoni <adgibertoni@gmail.com>
2022-10-26 16:47:43 -0300

    busqueda lista desde index, app player completa

--- nba_blog/home/urls.py
+++ nba_blog/home/urls.py
@@ -5,5 +5,5 @@ from home import views
 app_name = "home"
 urlpatterns = [
     path("", views.index, name="index"),
-    #path("search/", views.search, name="search"),
+    path("search/", views.search, name="search"),
 ]
--- nba_blog/home/views.py
+++ nba_blog/home/views.py
@@ -1,6 +1,8 @@
 from django.db.models import Q
 from django.shortcuts import render
 
+from player.models import Player
+
 def index(request):		
 		return render(
 			request=request,
@@ -8,3 +10,22 @@ def index(request):
 			template_name="home/index.html",
 		)
         
+def search(request):
+    search_param = request.GET["search_param"]
+    print("search: ", search_param)
+    context_dict = dict()
+    if search_param:
+        query = Q(name__contains=search_param)
+        query.add(Q(last_name__contains=search_param), Q.OR)
+        players = Player.objects.filter(query)
+        context_dict.update(
+            {
+                "players": players,
+                "search_param": search_param,
+            }
+        )
+    return render(
+        request=request,
+        context=context_dict,
+        template_name="home/index.html",
+    )
